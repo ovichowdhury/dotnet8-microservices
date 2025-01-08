7609c3ca0f50945546def97322e90fa1cd18e98e
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-01-08 18:50:41 +0600

    feat: delete product api added

--- Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -7,7 +7,7 @@ namespace Catalog.API.Products.UpdateProduct
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSuccess);
-    internal class UpdateProductCommandHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    internal class UpdateProductHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
