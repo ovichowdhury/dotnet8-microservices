09446c2bb4da4c69db1d25f1a41cd05c903eae6e
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-01-08 13:24:13 +0600

    feat: get product by id api added

---
+++ Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using ClassLib.CQRS;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductByIdHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"GetProductsByIdHandler.Handle Called with {query}");
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if (product is null)
+            {
+                throw new ProductNotFoundException("Product not Found");
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}
