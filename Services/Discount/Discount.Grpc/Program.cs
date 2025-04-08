37ba0aa182ed6f45883792142ef25d62a3048bd2
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-04-08 17:18:53 +0600

    feat: discount entity framework config added for sqlite

--- Services/Discount/Discount.Grpc/Program.cs
+++ Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,15 @@
+using Discount.Grpc.Data;
 using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection(); // Enable gRPC reflection
+builder.Services.AddDbContext<DiscountContext>(opts => { 
+    opts.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
 
