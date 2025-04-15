52b4b053d00ca12101125997238589d138bb6c88
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-04-15 13:55:56 +0600

    feat: create discount grpc added

--- Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -18,5 +18,9 @@ public class DiscountContext : DbContext
             new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
             new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
         );
+
+        modelBuilder.Entity<Coupon>()
+            .HasIndex(c => c.ProductName)
+            .IsUnique();
     }
 }
---
+++ Services/Discount/Discount.Grpc/Migrations/20250415074601_ProductNameUniqueConstrainsAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductNameUniqueConstrainsAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Coupons_ProductName",
+                table: "Coupons",
+                column: "ProductName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Coupons_ProductName",
+                table: "Coupons");
+        }
+    }
+}
