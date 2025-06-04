ae617ccb3d4eb943d784db5232a1b464b7531f65
ovichowdhury <ovi.chowdhury@hotmail.com>
2025-06-04 17:55:33 +0600

    feat: order domain generic entity added

---
+++ Services/Ordering/Ordering.Domain/Abstractions/IEntity.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions;
+
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
+
+public interface IEntity<T> : IEntity
+{
+    public T Id { get; set; }
+}
